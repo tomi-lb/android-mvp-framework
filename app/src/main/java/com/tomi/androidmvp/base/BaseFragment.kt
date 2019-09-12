9cb36b6da651807b214a2d3b6ebe2c9a89bce10d
Tomi <Tomi@abc.com>
2019-09-12 18:12:24 +0800

    add BaseFragment.kt

---
+++ app/src/main/java/com/tomi/androidmvp/base/BaseFragment.kt
@@ -0,0 +1,46 @@
+package com.tomi.androidmvp.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+/**
+ * Fragment lifecycle =>
+ * onAttach -> onCreate -> onCreateView -> onActivityCreated -> onStart ->
+ * onResume -> onPause -> onStop -> onDestroyView -> onDestory -> onDetach
+ */
+abstract class BaseFragment<K : IBaseView, V : BasePresenter<K>> : Fragment(), IBaseView {
+    var mPresenter: V? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        return inflater.inflate(getLayout(), null)
+    }
+
+    //onCreateView是创建的时候调用，onViewCreated是在onCreateView后被触发的事件，前后关系
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initFrag()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mPresenter = createPresenter()
+        mPresenter?.attachView(this as K)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mPresenter?.detachView()
+    }
+
+    abstract fun getLayout(): Int
+    abstract fun initFrag()
+    abstract fun createPresenter(): V
+}
